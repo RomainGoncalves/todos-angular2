65b3a83765a8c64538ab7bb4591594322b3f0b2a
RomainGoncalves <goncalves.romain@gmail.com>
2017-02-03 01:54:47 +0100

    Has a dynamic display of Pending and Completed tasks

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -3,22 +3,41 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class TodosService {
 
-  public tasks;
+  tasks = [
+    {
+      title: 'Switch on the power',
+      completed: false
+    },
+    {
+      title: 'Enter password',
+      completed: false
+    },
+    {
+      title: 'Use the computer',
+      completed: false
+    }
+  ];
 
-  constructor() {
-    this.tasks = [
-      {
-        title: 'Switch on the power',
-        completed: false
-      },
-      {
-        title: 'Enter password',
-        completed: false
-      },
-      {
-        title: 'Use the computer',
-        completed: false
-      }
-    ];
+  completedTasks(){
+
+    let completed = [];
+
+    for(let task of this.tasks){
+      if(task.completed) completed.push(task);
+    }
+
+    return completed;
   }
+
+  pendingTasks(){
+    let pending = [];
+
+    for(let task of this.tasks){
+      if(!task.completed) pending.push(task);
+    }
+
+    return pending;
+  }
+
+  constructor() {}
 }
